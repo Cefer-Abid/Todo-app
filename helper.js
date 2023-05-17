bf0b791f20a34f232b07d3a620e9fb45d468c819
ceferAbid <ceferabid@gmail.com>
2023-05-17 21:23:37 +0400

    added js structur

--- helper.js
+++ helper.js
@@ -2,6 +2,13 @@ const iconMoon = document.querySelector(".icon--moon");
 const iconSun = document.querySelector(".icon--sun");
 const headerImgLightMode = document.querySelector(".header__img--light-mode");
 const headerImgDarkMode = document.querySelector(".header__img--dark-mode");
+const todoList = document.querySelector(".todo--list");
+const countTodoList = document.querySelector(".count--todo-list");
+const checkboxIconInput = document.querySelector(".checkbox--icon__input");
+const checkboxInput = document.querySelector(".checkbox__input");
+const searchInput = document.querySelector(".search-input");
+
+
 
 export const toggleDarkMode = function (darkMode) {
   if (darkMode) {
@@ -18,4 +25,66 @@ export const toggleDarkMode = function (darkMode) {
     headerImgLightMode.classList.remove("hidden");
     headerImgDarkMode.classList.add("hidden");
   }
+};
+
+export const updateTodoList = function (data) {
+  todoList.innerHTML = "";
+  const str = data
+    .map((todo, i) => {
+      return `
+    <li class="todo__item">
+      <div class="todo-main-inside">
+        <div class="checkbox" data-checked="false" data-index="${i}">
+          <svg class="checkbox--icon hidden" xmlns="http://www.w3.org/2000/svg" width="11" height="9"><path fill="none" stroke="#FFF" stroke-width="2" d="M1 4.304L3.696 7l6-6"/></svg>
+        </div>
+        <span class="todo__title ${todo.checked && "completed-todo"}">
+          ${todo.title}</span>
+      </div>
+      <svg  class="todo__delete-icon icon" data-index="${i}" xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>
+    </li>
+  `;
+    })
+    .join("");
+
+  todoList.insertAdjacentHTML("beforeend", str);
+
+  // Active todo items count
+  const activeTodoItems = data.filter((todo) => !todo.checked).length;
+  countTodoList.textContent = activeTodoItems;
+};
+
+export const setLocalStorage = function (itemName, item) {
+  localStorage.setItem(itemName, JSON.stringify(item));
+};
+
+export const getLocalStorage = function (item) {
+  return JSON.parse(localStorage.getItem(item));
+};
+
+export const displayAllTodos = function (data) {
+  const allTodos = data.filter((todo) => todo);
+  updateTodoList(allTodos);
+};
+
+export const displayActiveTodos = function (data) {
+  const activeTodos = data.filter((todo) => !todo.checked);
+  updateTodoList(activeTodos);
+};
+
+export const displayCompletedTodos = function (data) {
+  const completedTodos = data.filter((todo) => todo.checked);
+  updateTodoList(completedTodos);
+};
+
+export const displayClearedTodos = function (data) {
+  data = [];
+  updateTodoList(data);
+  setLocalStorage("data", data);
+};
+
+export const getInputStatus = function () {
+  checkboxInput.dataset.checked = "false";
+  checkboxInput.classList.remove("active-checkbox");
+  checkboxIconInput.classList.add("hidden");
+  searchInput.classList.toggle("completed-todo");
 };
--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-import { toggleDarkMode } from "/helper.js";
+// prettier-ignore
+import { toggleDarkMode, updateTodoList, setLocalStorage, getLocalStorage, displayAllTodos, displayActiveTodos, displayCompletedTodos,displayClearedTodos,getInputStatus
+} from "/helper.js";
 
 const content = document.querySelector(".content");
+const formEl = document.querySelector(".search-field");
+const searchInput = document.querySelector(".search-input");
+const checkboxInput = document.querySelector(".checkbox__input");
 const controlButton = document.querySelectorAll(".control-button");
-let darkMode = false;
+let darkMode, data;
 
-const getMode = function () {
-  const darkMode = JSON.parse(localStorage.getItem("dark-mode"));
-  toggleDarkMode(darkMode);
+const init = function () {
+  // init value
+  darkMode = false;
+  data = [];
+
+  // Get dark mode from local storage
+  const activeDarkMode = getLocalStorage("dark-mode");
+  toggleDarkMode(activeDarkMode);
+
+  // Get data from local storage
+  const localData = getLocalStorage("data");
+  if (!localData) return;
+  data = localData;
+  updateTodoList(data);
 };
-getMode();
+init();
 
 content.addEventListener("click", function (e) {
   const el = e.target;
@@ -18,7 +34,7 @@ content.addEventListener("click", function (e) {
     darkMode = !darkMode;
     toggleDarkMode(darkMode);
     // Send to Local Storage
-    localStorage.setItem("dark-mode", darkMode);
+    setLocalStorage("dark-mode", darkMode);
   }
 
   // Toggle Checkbox icon
@@ -43,8 +59,64 @@ content.addEventListener("click", function (e) {
     }
   }
 
-  if(el.closest(".control-button")){
-    controlButton.forEach(btn=>btn.classList.remove("actived--control-btn"))
-    el.closest(".control-button").classList.add("actived--control-btn")
+  // Footer active btn
+  if (el.closest(".control-button")) {
+    controlButton.forEach((btn) => btn.classList.remove("active--control-btn"));
+    el.closest(".control-button").classList.add("active--control-btn");
   }
+
+  // Delete todo item
+  if (el.closest(".todo__delete-icon")) {
+    const deleteIndex = +el.closest(".todo__delete-icon").dataset.index;
+    data.splice(deleteIndex, 1);
+
+    updateTodoList(data);
+    setLocalStorage("data", data);
+  }
+
+  // Complete todo item
+  if (el.closest(".checkbox")) {
+    const completedIndex = +el.closest(".checkbox").dataset.index;
+    const completedTodo = data[completedIndex];
+    if (!completedTodo) return searchInput.classList.toggle("completed-todo");
+    completedTodo.checked = !completedTodo.checked;
+
+    updateTodoList(data);
+    setLocalStorage("data", data);
+  }
+
+  // Control buttons
+  if (el.closest(".footer")) {
+    if (el.closest(".btn-all")) displayAllTodos(data);
+    if (el.closest(".btn-active")) displayActiveTodos(data);
+    if (el.closest(".btn-completed")) displayCompletedTodos(data);
+    if (el.closest(".btn-clear-all")) displayClearedTodos(data);
+  }
+});
+
+
+
+formEl.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  // Get input checkbox status
+  let inputStatus = checkboxInput.dataset.checked === "false" ? false : true;
+  getInputStatus();
+
+  // Add new todo data
+  const newTodo = searchInput.value;
+  const obj = {
+    title: newTodo,
+    checked: inputStatus ? true : false,
+  };
+  data.push(obj);
+
+  // Update todo list view
+  updateTodoList(data);
+
+  // Send data local storage
+  setLocalStorage("data", data);
+
+  // Clean input
+  searchInput.value = "";
 });
